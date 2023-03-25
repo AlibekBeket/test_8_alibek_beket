9ea2295eca338e923ae75bb832d9017f5c8b57a8
Alibek Beket <alibek1beket@gmail.com>
2023-03-25 15:22:06 +0600

    изменил полностью функцию детального просмотра страницы продукта(изменил на модель review и добавил пагинацию) и стилизовал отзывы

--- app/feedback/views/product.py
+++ app/feedback/views/product.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from feedback.models import Product
+from feedback.models import Product, Review
 
 from feedback.forms import ProductForm
 
@@ -14,9 +15,22 @@ class ProductListView(ListView):
     paginate_orphans = 1
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(ListView):
     template_name = 'product_detail.html'
-    model = Product
+    model = Review
+    context_object_name = 'reviews'
+    paginate_by = 5
+    paginate_orphans = 1
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['product'] = get_object_or_404(Product, pk=self.kwargs['pk'])
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(product=get_object_or_404(Product, pk=self.kwargs['pk']))
+        return queryset
 
 
 class ProductAddView(CreateView):
